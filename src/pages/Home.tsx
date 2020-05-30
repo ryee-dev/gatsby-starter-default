1d68b6a37334d4195f38dcdb6d397603488b5ae6
roger yee <ryee.epicodus@gmail.com>
2020-05-29 20:46:12 -0400

    reverts breaking changes and updates deps

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Flex, Text } from 'theme-ui';
 import { motion } from 'framer-motion';
